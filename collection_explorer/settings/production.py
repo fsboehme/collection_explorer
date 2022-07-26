fce1dbaff8128a9ea57cef92ff7c2e93ce24ed00
Felix Boehme <fsboehme@gmail.com>
2022-07-26 12:24:14 -0400

    production (digital ocean apps) settings

--- collection_explorer/settings/production.py
+++ collection_explorer/settings/production.py
@@ -1,10 +1,36 @@
-from .base import *
+import os
+import sys
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+from .base import *
+import dj_database_url
+
+DEBUG = os.getenv("DEBUG", "False") == "True"
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+TEMPLATE_DEBUG = DEBUG
 
 SITE_NAME = 'Collection Explorer'
 SITE_DOMAIN = 'collection-explorer.travelix.earth'
 
-ALLOWED_HOSTS += [f'.{SITE_DOMAIN}']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+
 WSGI_APPLICATION = 'django_project.wsgi.prod.application'
+
+from django.core.management.utils import get_random_secret_key
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
