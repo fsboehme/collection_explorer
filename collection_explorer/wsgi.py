09c2f3749f6e0a95e8dae2ef0aa16278b79412c3
Felix Boehme <fsboehme@gmail.com>
2022-07-26 16:09:33 -0400

    dotenv

--- collection_explorer/wsgi.py
+++ collection_explorer/wsgi.py
@@ -8,11 +8,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
-
 import dotenv
+
 from django.core.wsgi import get_wsgi_application
 
-dotenv.read_dotenv(override=True)
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'), override=True)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'collection_explorer.settings')
 
